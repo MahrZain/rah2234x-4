670af986790609028e2e6e1b951d214ad34e326d
Mehar Zain <zainzainpubg1122@gmail.com>
2024-12-12 10:04:39 +0500

    op

--- data/models.py
+++ data/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
-class CustomUser(models.Model):
 
+class CustomUser(models.Model):
     USER_CHOICE = (
         ('Session', 'Session'),  
         ('Click', 'Click'),
     )
+
     class Meta:
         verbose_name_plural = 'Users Data'
-    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
-    IP = models.GenericIPAddressField(max_length=255)  
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    IP = models.GenericIPAddressField(protocol='both', unpack_ipv4=True)  # Supports all IPs (IPv4 and IPv6)
     type = models.CharField(max_length=255, choices=USER_CHOICE) 
     created_at = models.DateTimeField(auto_now_add=True)  
-    
 
     def __str__(self):
-        return f"{self.IP} - {self.type}"  
+        return f"{self.IP} - {self.type}"
